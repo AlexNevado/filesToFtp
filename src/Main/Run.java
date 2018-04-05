1aaaecd17f55038fe1f1423bc2e6ac6348ca3bb2
AlexNevado <pailex95@gmail.com>
2018-04-05 22:44:46 +0200

    Creada la clase de compresion

--- src/Main/Run.java
+++ src/Main/Run.java
@@ -1,14 +1,22 @@
 package Main;
 
+import Utilities.ZipCompress;
+
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Dark_Trainer on 05/04/2018.
  */
 public class Run {
     public static void main(String[] args) {
-        File f = new File("D:\\Media\\");
-        System.out.print(printDirectoryTree(f));
+
+        ZipCompress.compress("D:\\holaToxic");
+
     }
 
     /**
